9ec0b78c1d1a0d92c565fe87d8566b0496b34940
Ali Elsayed <60029669+BraveCake@users.noreply.github.com>
2023-10-27 22:10:40 +0200

    Add stealth typing

--- Source.js
+++ Source.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord Custom
 // @namespace    http://tampermonkey.net/
-// @version      5
+// @version      6
 // @description  try to take over the world!
 // @author       BraveKek
 // @match       https://discord.com/*
@@ -9,24 +9,31 @@
 // @require http://code.jquery.com/jquery-3.5.1.min.js
 // @grant        none
 // @run-at       document-end
+// @grant           GM_webRequest
+// @webRequest   {"selector":"https://discord.com/api/v9/channels/*/typing","action":"cancel"}
 // ==/UserScript==
 
-/* Avoid modifying any line has not comments next to it comment lines start with // or /* */
-var $ = window.jQuery;
+
+
 const nitro = false; // set it to true if you use nitro it will disable copying emojis into your clipboard
-const none = 'none';
 const font_type = "cursive" // customize a font type for the entire page. only web safe fonts are supported, you may google "web safe fonts" for more details
 const brightness = 100; // controls the brighness of the page, minimum value is 0 and the maximum is 100.
-const imageUrl = "https://cdn.discordapp.com/attachments/810213552912203827/1057332241757372437/tumblr_7a9e0547a4db9a50a90693425007d682_4285046f_1280.gif"; //change the url to url of the image which you want to use as a background but before that make sure that you have all color values set as transparent or none
+const imageUrl = "https://cdn.discordapp.com/attachments/815610760205565952/1094976704281329695/peakpx_1.jpg"; //change the url to url of the image which you want to use as a background but before that make sure that you have all color values set as transparent or none
 /* the image may not work if you're using unsupported/ not well known website url so we recommend uploading images on discord to avoid experiencing unpleasant issues */
 /* previous image : https://media.discordapp.net/attachments/691199564262146061/972072549846483004/unknown.png?width=1145&height=644" */
 const cursor = "https://cdn.discordapp.com/attachments/691199564262146061/913052994013122640/pngegg.png"; //image of the custom cursor the size preferred to be 128px*128px make sure it's uploaded on discord because other websites might get blocked automatically by discord
-const auto_track = "hsl(1000,calc(var(--saturation-factor, 6)*9%),9%)";
 const online = '255,111,255';//rgb value of online status leave it empty do use the default
 const offline = '255,255,255'; //rgb value of offline status leave it empty to use the default
 const dnd = '0,0,250'; //rgb value of dont disturb status leave it empty to use the default
 const notificationImage="https://media.discordapp.net/attachments/810213552912203827/1033314055634559026/pngwing.com.png?width=676&height=619"//URL of the image that will appear when you click on any emoji it is just for notification purpose
 var hideButton=1; //set it to zero if you dont want hide friends button
+
+
+/* Avoid modifying any line has not comments next to it comment lines start with // or /* */
+
+var $ = window.jQuery;
+const none = 'none';
+const auto_track = "hsl(1000,calc(var(--saturation-factor, 6)*9%),9%)";
 (function go(){
     var successNotifier= document.createElement("div");
     successNotifier.style.cssText ="position:absolute; left:90%; top:90%; width:100px; height:100px;background-size:cover;border-radius:50%;visibility:hidden;"+"background-image:url('"+notificationImage+"')";
@@ -41,8 +48,8 @@ var hideButton=1; //set it to zero if you dont want hide friends button
            let hide= document.createElement("button");
 hide.innerHTML = "hide";
             hide.style["border-radius"]="30px";
-            hide.onclick = ()=> $(".scroller-WSmht3.thin-RnSY0a.scrollerBase-1Pkza4.fade-27X6bG").toggle();
-            $(".searchBar-3TnChZ")[0].appendChild(hide);
+            hide.onclick = ()=> $(".scroller__4b984.thin_b1c063.scrollerBase_dc3aa9.fade_ba0fa0").toggle();
+            $(".searchBar_e4ea2a")[0].appendChild(hide);
             hideButton=-1;
 
         }
@@ -53,62 +60,69 @@ hide.innerHTML = "hide";
  'use strict';
    $('head').append('<style>'+
                     '.theme-dark {'+
-                    '--background-primary:rgba(0,0,0,0.2)        ;'+ //compositor color 0
-                    '--background-secondary:transparent         ;'+ //compository color 1
-                    '--background-tertiary:none           ;'+ //compository color  2
-                   ' --background-accent:none            ; '+ //compository color  3
-                   ' --background-secondary-alt:none          ;'+ //compository color  4
-                    ' --text-muted:white                       ;'+  // color of placeholders, some headers and message time
-                    '--text-normal:white                         ;'+ // text color of the messages
-                    '--text-muted:white                          ;'+ // color of placeholders, some headers and message time
-                    '--channeltextarea-background:transparent;' + //color of message typing box
-                    '--scrollbar-auto-thumb:rgba(46, 49, 49, 1)              ;'+ //scroll button color
-                    '--scrollbar-auto-track:'+none+'              ;'+//scroll line use auto_track to keep it visible or none to disable it unfortunately it's not possible to customize it yet
+                    '--background-primary:rgba(0,0,0,0.2) !important        ;'+ //compositor color 0
+                    '--background-secondary:transparent         !important;'+ //compository color 1
+                    '--background-tertiary:none           !important;'+ //compository color  2
+                   ' --background-accent:none            !important; '+ //compository color  3
+                   '--bg-overlay-app-frame:none          !important; '+
+                   ' --background-secondary-alt:none          !important;'+ //compository color  4
+                    ' --text-muted:white                       !important;'+  // color of placeholders, some headers and message time
+                    '--text-normal:white                         !important;'+ // text color of the messages
+                    '--text-muted:white                          !important;'+ // color of placeholders, some headers and message time
+                    '--channeltextarea-background:transparent  !important;' + //color of message typing box
+                    '--scrollbar-auto-thumb:rgba(46, 49, 49, 1)              !important;'+ //scroll button color
+                    '--scrollbar-auto-track:'+none+'              !important;'+//scroll line use auto_track to keep it visible or none to disable it unfortunately it's not possible to customize it yet
                     '}' +
                      '.applicationStore-1pNvnv{'+
-                    'background-color:transparent                   ;' +//color of nitro page recommendation : use transparent instead of none here
+                    'background-color:transparent                   !important;' +//color of nitro page recommendation : use transparent instead of none here
                     '}'+
                     '.scrollableContainer-15eg7h{'+
-                    'background:transparent;'+
+                    'background:transparent  !important;'+
                     '}'
                     +
+                    '.emojiItemDisabled__36cbf{' +
+                    'filter: grayscale(0%) !important;'+
+                    '}'
+
+                    +
                      '.scrollableContainer-2NUZem{'+
-                    'background:transparent                         ;'+ //Deprecated (might not work in the new version) typing-box
+                    'background:transparent                         !important;'+ //Deprecated (might not work in the new version) typing-box
                     '}'+
                       '.tabBody-3YRQ8W{'+    //new typing
-                    'background-image:0%;'+
+                    'background-image:0%  !important;'+
                     '}'+
                                           '.theme-dark .container-2cd8Mz{'+    //the new friends page
-                    'background-color:transparent;'+
+                    'background-color:transparent  !important;'+
                     '}'+
                                                               '.applicationStore-2nk7Lo{'+    //the new nitro page
-                    'background-color:transparent;'+
+                    'background-color:transparent !important;'+
                     '}'+
-                    '::selection {background-color:#982C3B;}'  //color of any selection done by mouse
+                    '::selection {background-color:#982C3B !important;}'  //color of any selection done by mouse
                     +
+                    '.app__00c92{ background:none !important;}'+
                   '.theme-dark .container-1D34oG{'+
-                    'background-color:transparent                   ;}'+ //color of friends page(Deprecated (might not work in the new version)  recommendation : use transparent instead of none here
+                    'background-color:transparent                   !important;}'+ //color of friends page(Deprecated (might not work in the new version)  recommendation : use transparent instead of none here
                     '.ready-36e6Vk{'+
 
                     '}'+
                       '.scroller-1Bvpku{'+
-                    'background-color:rgba(46, 49, 49, 0.1)         ;}'+ //Server lists vertical bar color
+                    'background-color:rgba(46, 49, 49, 0.1)        !important ;}'+ //Server lists vertical bar color
                      '.members-1998pB{'+
-                    'background-color:transparent                    ;}'+ //Server Members List color
+                    'background-color:transparent                    !important;}'+ //Server Members List color
 
                     '.embedFull-2tM8--{'+
-    'border-left: 4px solid black                                    ;'+
-   ' background: transparent                                         ;}'+ //embed box background color
+    'border-left: 4px solid black                                    !important;'+
+   ' background: transparent                                         !important;}'+ //embed box background color
 
                    '.chatContent-a9vAAp{'+
-   ' background-color: transparent                                    ;}'+ //color of middle section of the app where send messages being sent
+   ' background-color: transparent                                    !important;}'+ //color of middle section of the app where send messages being sent
                    +
                     '.container-1r6BKw.themed-ANHk51{'+
-                    'background-color: transparent                    ;}' // customization of the upper most bar of the page which contains the channel name and search bar
+                    'background-color: transparent                    !important;}' // customization of the upper most bar of the page which contains the channel name and search bar
                    +
-                   'img,.clickable-3Ya1ho {cursor:'+ 'url('+cursor+'),auto}'+
-                    '.theme-dark .background-fkKrXt{background:none;}'+
-                 '.emojiItemDisabled-3VVnwp,.stickerUnsendable-PkQAxI{-webkit-filter;filter:none;}'+
+                   'body,img,.clickable-3Ya1ho {cursor:'+ 'url('+cursor+'),auto !important}'+
+                    '.theme-dark .background-fkKrXt{background:none !important;}'+
+                 '.emojiItemDisabled-3VVnwp,.stickerUnsendable-PkQAxI{-webkit-filter;filter:none !important;}'+
                  '</style>');
 /* by bravecake */
      $('body').css('--text-link','pink'); //color of urls
@@ -116,12 +130,11 @@ hide.innerHTML = "hide";
     $('body').css('background-size', 'cover');
     $('body').css('filter','brightness('+brightness+'%)');
     $('body').css('font-family',font_type);
-    $('body').css('cursor', 'url(https://media.discordapp.net/attachments/691199564262146061/911176926767480862/7-70485_computer-mouse-cursor-free-png-image-custom-mouse.png), pointer;');
-    $('body').css('cursor','url('+cursor+'),auto')
-    $(document).ready(function() { setInterval(function () {hb();for (var l of document.querySelectorAll('button.emojiItem-277VFM,div.assetWrapper-2hzITV')){l.addEventListener('click',function () {if(nitro)return; var t= this.firstChild.src[this.firstChild.src.length-4]=='='?this.firstChild.src.slice(0,this.firstChild.src.length-3)+'48':this.firstChild.src.slice(0,this.firstChild.src.length-1)+'8'; if(this.firstChild.dataset.type=='sticker')t=t.substr(0,t.length-2)+160;  navigator.clipboard.writeText(t.replace('&quality=lossles8','')); successNotifier.style.visibility="visible"; setTimeout(_=>successNotifier.style.visibility="hidden",2000); setTimeout(_=>$('.premiumPromoClose-Nuntxy,.upsellClose-72TQqH').click(),0); console.log(this.firstChild.src);});}let online_l =  $('rect[mask="url(#svg-mask-status-online)"]') ; for (let x of online_l) {if(online.length==0)break; x.setAttribute('fill','rgb('+online+')');} online_l =  $('rect[mask="url(#svg-mask-status-offline)"]') ; for (let x of online_l) {if(offline.length==0)break; x.setAttribute('fill','rgb('+offline+')');}online_l =  $('rect[mask="url(#svg-mask-status-dnd)"]') ; for (let x of online_l) {if(dnd.length==0)break; x.setAttribute('fill','rgb('+dnd+')');}},1000); });
+    $('body').css('cursor','url('+cursor+'),auto');
+    $(document).ready(function() { setInterval(function () {hb();for (var l of document.querySelectorAll('button.emojiItemDisabled__36cbf')){l.addEventListener('click',function () {if(nitro)return; var t= this.firstChild.src[this.firstChild.src.length-4]=='='?this.firstChild.src.slice(0,this.firstChild.src.length-3)+'48':this.firstChild.src.slice(0,this.firstChild.src.length-1)+'8'; if(this.firstChild.dataset.type=='sticker')t=t.substr(0,t.length-2)+160;  navigator.clipboard.writeText(t.replace('&quality=lossles8','')); successNotifier.style.visibility="visible"; setTimeout(_=>successNotifier.style.visibility="hidden",2000); setTimeout(_=>$('.premiumPromoClose__11b8f,.upsellClose-72TQqH').click(),0); console.log(this.firstChild.src);});}let online_l =  $('rect[mask="url(#svg-mask-status-online)"]') ; for (let x of online_l) {if(online.length==0)break; x.setAttribute('fill','rgb('+online+')');} online_l =  $('rect[mask="url(#svg-mask-status-offline)"]') ; for (let x of online_l) {if(offline.length==0)break; x.setAttribute('fill','rgb('+offline+')');}online_l =  $('rect[mask="url(#svg-mask-status-dnd)"]') ; for (let x of online_l) {if(dnd.length==0)break; x.setAttribute('fill','rgb('+dnd+')');}},1000); });
 
 
-    //setInterval(function () {for (l of document.querySelectorAll('div.graphicPrimary-1I8l6r img, .emoji')){l.addEventListener('click',function () {var t= this.src[this.src.length-4]=='='?this.src.slice(0,this.src.length-3)+'48':this.src.slice(0,this.src.length-1)+'8'; navigator.clipboard.writeText(t); console.log(this.src);});}},1000)
+   
 
 
 
